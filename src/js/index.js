7c89ff69921012e5e108fa5cb089774c26be8cb8
Victor Lopes <125214784+VictorLPSRD@users.noreply.github.com>
2023-11-12 22:11:16 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 /* 
-  O que precisamos fazer? - quando clicar no botão do personagem na lista, temos que marcar o botão como selecionado e mostrar o personagem correspondente
+  Clicar no botão do personagem(lista) >> marcar o botão como selecionado >> mostrar o personagem selecionado
 
-    OBJETIVO 1 - quando clicar no botão do personagem na lista, marcar o botao como selecionado 
+    OBJETIVO 1 - Clicar no botão do personagem >> marcar como selecionado 
         passo 1 - pegar os botões no JS pra poder verificar quando o usuário clicar em cima de um deles
         passo 2 - adicionar a classe "selecionado" no botão que o usuário clicou
         passo 3 - verificar se já existe um botão selecionado, se sim, devemos remover a seleção dele 
